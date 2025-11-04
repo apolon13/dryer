5db66f895508668fbca8e69585376f423dd8d478
pankov <pankov@tutu.tech>
2025-11-04 21:46:43 +0300

    refactoring

--- src/dryer/fan/mod.rs
+++ src/dryer/fan/mod.rs
@@ -10,20 +10,16 @@ impl<'a> Fan<'a> {
     pub fn new(pwm: LedcDriver<'a>) -> Self {
         Fan { pwm }
     }
-
-    fn next_step_for_dryer(&self, direction: FanMode) -> u32 {
-        let max = self.pwm.get_max_duty();
-        match direction {
-            FanMode::Middle => (max as f64 * (1.0 - 30.0 / 100.0)) as u32,
-            FanMode::Max => max,
-            FanMode::Off => 0,
-        }
-    }
 }
 
 impl<'a> FanSpeedRegulator for Fan<'a> {
     fn speed(&mut self, mode: FanMode) -> Result<(), Error> {
-        self.pwm.set_duty(self.next_step_for_dryer(mode))?;
+        let max = self.pwm.get_max_duty();
+        self.pwm.set_duty(match mode {
+            FanMode::Middle => (max as f64 * (1.0 - 30.0 / 100.0)) as u32,
+            FanMode::Max => max,
+            FanMode::Off => 0,
+        })?;
         Ok(())
     }
 }
